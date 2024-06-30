e8b30c835b070cd0d995bc9d85852f3f8a551e3f
WenHuang <terry20431@gmail.com>
2024-06-30 18:05:32 +0800

    first upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+#print("hellow world")
+
+name = "hellow  world"
+#positions = [i for i, letter in enumerate(name) if letter == 'l']
+for index, character in enumerate(name):
+    print(index, character)
+
+#print(positions)
